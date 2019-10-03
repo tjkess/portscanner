855ed72ad60bcd6ce58b1a6ab08baf41459dc0aa
TJ <kessingertj@gmail.com>
2019-10-03 17:27:26 -0600

    added stuff

--- scanner.py
+++ scanner.py
@@ -7,12 +7,12 @@ def print_info(port, state):
 
 
 def tcp(t, p):
-    print("starting tcp scan on target %s on ports %s" %(t, p))
+    print("starting tcp scan on target %s on ports %s" % (t, p))
     source = RandShort()
     info = ""
     for target in t:
         for port in p:
-            packet = sr1(IP(dst=target)/TCP(sport=source, dport=port, flags="FPU"), timeout=1)
+            packet = sr1(IP(dst=target)/TCP(sport=source, dport=port, flags="FPU"), timeout=1, verbose=1)
             if packet != None:
                 if packet.haslayer(TCP):
                     if packet[TCP].flags == 20:
@@ -28,11 +28,11 @@ def tcp(t, p):
 
 
 def udp(t, p):
-    print("start udp scan on ports %s" % p)
+    print("starting udp scan on ports %s ...." % p)
     info = ""
     for target in t:
         for port in p:
-            packet = sr1(IP(dst=target)/UDP(sport=port, dport=port), timeout=2)
+            packet = sr1(IP(dst=target)/UDP(sport=port, dport=port), timeout=1, verbose=1)
             if packet == None:
                 print_info(port, "open or filtered")
             else:
@@ -47,34 +47,32 @@ def udp(t, p):
 
 
 parser = argparse.ArgumentParser("Welcome to the port scanner")
-parser.add_argument("-t", "--target", help="specify the ip address of your target", required=True, nargs="+")
+parser.add_argument("-t", "--target", help="specify the ip address of your target", nargs="+")
 parser.add_argument("-p", "--ports", help="specify the ports to be scanned", nargs="+", type=int)
 parser.add_argument("-f", "--file", help="include a txt file with targets separated by commas")
 parser.add_argument("-s", "--scan", help="specify the type of scan eg(tcp, udp, icmp)", required=True)
 
 inputs = parser.parse_args()
 
+if inputs.target == None and inputs.file == None:
+    print("specify -t or -f")
+    exit()
 if inputs.file:
     target = []
     f = open(inputs.file, "r")
-    file_content = f.read()
-    ip = ""
-    for letter in file_content:
-        if letter == ',':
-            target.append(ip)
-            ip = ""
-            continue
-        ip += letter
+    file_content = f.readline()
+    file_content = file_content.replace('\n', '')
+    file_content = file_content.split(',')
+    for i in range(len(file_content)):
+        target.append(file_content[i])
 else:
     target = inputs.target
 if inputs.ports:
     ports = inputs.ports
 else:
-    ports = range(1, 1024)
+    ports = range(1, 1000)
 scan = inputs.scan.lower()
 if scan == 'udp':
     udp(target, ports)
 elif scan == 'tcp':
-    print (tcp(target, ports))
-
-
+    tcp(target, ports)
